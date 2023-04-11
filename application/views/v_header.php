03c75eaea25fecfc629f636ffc886a7156cfdd57
sandymaulan664 <sandymaulan664@gmail.com>
2023-04-11 15:34:44 +0700

    pertemuan 5

--- application/views/v_header.php
+++ application/views/v_header.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-    <div id=”wrapper”>
+    <div  id="wrapper">
         <header>
             <hgroup>
                 <h1>RentalBuku.net</h1>
@@ -18,11 +18,11 @@
             </hgroup>
             <nav>
                 <ul>
-                    <li><a href=”<?php echo base_url() . 'index . php / web' ?>”>Home</a></li>
-                    <li><a href=”<?php echo base_url() . 'index . php / web / about' ?>”>About</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web'?>">Home</a></li>
+                    <li><a href="<?php echo base_url().'index.php/web/about'?>">About</a></li>
                 </ul>
             </nav>
-            <div class=”clear”></div>
+            <div class="clear"></div>
         </header>
 
 </body>
